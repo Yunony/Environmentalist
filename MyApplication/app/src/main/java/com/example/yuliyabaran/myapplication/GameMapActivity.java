d56cf19202a5552647be87e83d624846b691a256
Brian Park <brokenazn@gmail.com>
2014-10-11 13:34:33 -0700

    fixed authentication errors
    modified xml files after issuing code from google console

--- MyApplication/app/src/main/java/com/example/yuliyabaran/myapplication/GameMapActivity.java
+++ MyApplication/app/src/main/java/com/example/yuliyabaran/myapplication/GameMapActivity.java
@@ -7,7 +7,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import android.util.Log;
 public class GameMapActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -42,6 +42,7 @@ public class GameMapActivity extends FragmentActivity {
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
+
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
